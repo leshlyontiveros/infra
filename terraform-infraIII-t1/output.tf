be339c6973f485ff6981e917e7cfc5bad4620a52
Leshly Ontiveros <85622591+leshlyontiveros@users.noreply.github.com>
2022-11-23 20:48:51 -0300

    Add files via upload

---
+++ terraform-infraIII-t1/output.tf
@@ -0,0 +1,10 @@
+output "instancia_ec2_k8s_publico" {
+  value = aws_instance.leshly_ec2_k8s.*.public_ip
+}
+output "instancia_ec2_k8s_private" {
+  value = aws_instance.leshly_ec2_k8s.*.private_ip
+}
+
+output "instancia_ec2_ansible" {
+  value = aws_instance.leshly_ec2_ansible.*.public_ip
+}
